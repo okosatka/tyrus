00f960668a7823161d38c5aa0b4b5737bc4d55ad
Ondrej Kosatka <ondrej.kosatka@oracle.com>
2014-07-21 18:29:50 +0200

    TYRUS-346: IllegalStateException when using proxy and JDK client fix.

--- client/src/main/java/org/glassfish/tyrus/client/TyrusClientEngine.java
+++ client/src/main/java/org/glassfish/tyrus/client/TyrusClientEngine.java
@@ -130,7 +130,8 @@ public class TyrusClientEngine implements ClientEngine {
     public UpgradeRequest createUpgradeRequest(URI uri, TimeoutHandler timeoutHandler) {
 
         switch (clientEngineState) {
-            case INIT: {
+            case INIT:
+            case FIRST_UPGRADE_REQUEST: {
                 ClientEndpointConfig config = (ClientEndpointConfig) endpointWrapper.getEndpointConfig();
                 this.timeoutHandler = timeoutHandler;
 
@@ -142,7 +143,7 @@ public class TyrusClientEngine implements ClientEngine {
                 UpgradeRequest upgradeRequest = clientHandShake.getRequest();
                 config.getConfigurator().beforeRequest(upgradeRequest.getHeaders());
 
-                clientEngineState = TyrusClientEngineState.FIRST_UPGRARE_REQUEST;
+                clientEngineState = TyrusClientEngineState.FIRST_UPGRADE_REQUEST;
                 return upgradeRequest;
             }
 
@@ -175,7 +176,7 @@ public class TyrusClientEngine implements ClientEngine {
     public ClientUpgradeInfo processResponse(final UpgradeResponse upgradeResponse, final Writer writer, final Connection.CloseListener closeListener) {
 
         switch (clientEngineState) {
-            case FIRST_UPGRARE_REQUEST:
+            case FIRST_UPGRADE_REQUEST:
             case IN_PROGRESS:
                 switch (upgradeResponse.getStatus()) {
                     case 101:
@@ -506,7 +507,7 @@ public class TyrusClientEngine implements ClientEngine {
         /**
          * Set after first {@link TyrusClientEngine#createUpgradeRequest(URI, TimeoutHandler)} invocation.
          */
-        FIRST_UPGRARE_REQUEST,
+        FIRST_UPGRADE_REQUEST,
 
         /**
          * In progress.
